28b041a9ff3c4227ba5ea9cfdfbecdc4b8ba241a
Luíza Bizoni <luiza.bizoni@mundiale.com.br>
2020-09-02 21:02:32 -0300

    Update second chapter archives study-types-cautionary-tales/observational_studies_and_experiments.R

---
+++ study-types-cautionary-tales/observational_studies_and_experiments.R
@@ -0,0 +1,24 @@
+# TYPES OF STUDIES
+# Observational study:
+## collect data in a way that does not directly interfere with how the data arise
+## only correlation can be inferred
+## typically uses random sampling, but not random assignment
+## the results are generalizable
+
+# Experiment:
+## randomly assign subjects to various treatments
+## causation can be inferred
+## typically uses random assignment, but not random sampling
+## the studies can be used to make causal conclusion, but it's not generalizable
+
+# RANDOM SAMPLING
+## At selection of subjects from population
+## helps generalizability of results
+
+# RANDOM ASSIGNMENT
+## At selection of subjects from population
+## helps infer causation from results
+
+
+# SIMPSON'S PARADOX
+## the inclusion of a third variable in the analysis can change the apparent relationship between the two other
